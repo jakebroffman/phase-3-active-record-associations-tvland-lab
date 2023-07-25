4ba291daadb6a3874b06c2da808063c0f3971efc
jakebroffman <jake.broffman1088@gmail.com>
2023-07-25 14:16:44 -0400

    commit tree 03e80f541dea37f4993cf29a812097b5139da32a

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
+require "pry"
+
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  belongs_to :network
+
+    def actors_list
+        characters.map do |character|
+            "#{character.actor.first_name} #{character.actor.last_name}"
+        end
+    end
 end
